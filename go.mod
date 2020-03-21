f6e63e6d3f957b3fdd8a8de3277d355398c709b2
Jorge L <jorge.alfmur@gmail.com>
2020-03-20 19:18:01 -0500

    setup mysql in docker-compose an install gorm

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/jorgeAM/api
 
 go 1.13
 
-require github.com/gorilla/mux v1.7.4
+require (
+	github.com/gorilla/mux v1.7.4
+	github.com/jinzhu/gorm v1.9.12 // indirect
+)
--- server.go
+++ server.go
@@ -1,24 +1,42 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
+type user struct {
+	Username string `json:"username"`
+}
+
 func main() {
 	r := mux.NewRouter()
-	r.HandleFunc("/user", getUser).Methods("GET")
+	r.HandleFunc("/user/{id}", getUser).Methods("GET")
 
 	s := http.Server{
 		Addr:    ":3000",
 		Handler: r,
 	}
 
+	log.Println("server is running on port 3000")
 	log.Fatal(s.ListenAndServe())
 }
 
 func getUser(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hola!"))
+	vars := mux.Vars(r)
+	id := vars["id"]
+	fmt.Println(id)
+	u := &user{Username: "steezclick"}
+	bytes, err := json.Marshal(u)
+
+	if err != nil {
+		log.Fatal("something got wrong to convert to json")
+		return
+	}
+
+	w.Write(bytes)
 }
