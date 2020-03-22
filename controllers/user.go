71edbad0ebf3e95f0701e0cb1ee426f08b4c0565
Jorge L <jorge.alfmur@gmail.com>
2020-03-22 16:10:36 -0500

    updapte user

--- controllers/user.go
+++ controllers/user.go
@@ -83,6 +83,68 @@ func NewUser(w http.ResponseWriter, r *http.Request) {
 	w.Write(bytes)
 }
 
+// UpdateUser updates user by id
+func UpdateUser(w http.ResponseWriter, r *http.Request) {
+	db := db.GetConnection()
+	defer db.Close()
+
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	u := models.User{}
+	db.Table("Users").First(&u, id)
+
+	if u.ID <= 0 {
+		m := &models.Response{
+			Code:    http.StatusNotFound,
+			Message: "user with id " + id + " does not exist",
+		}
+
+		utils.DisplayMessage(w, m)
+		return
+	}
+
+	err := json.NewDecoder(r.Body).Decode(&u)
+
+	if err != nil {
+		m := &models.Response{
+			Code:    http.StatusBadRequest,
+			Message: "something got wrong to parsing body",
+		}
+
+		utils.DisplayMessage(w, m)
+		return
+	}
+
+	err = db.Table("Users").Save(&u).Error
+
+	if err != nil {
+		m := &models.Response{
+			Code:    http.StatusBadRequest,
+			Message: "something got wrong to update user",
+		}
+
+		utils.DisplayMessage(w, m)
+		return
+	}
+
+	bytes, err := json.Marshal(&u)
+
+	if err != nil {
+		m := &models.Response{
+			Code:    http.StatusBadRequest,
+			Message: "something got wrong to convert to json",
+		}
+
+		utils.DisplayMessage(w, m)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	w.Write(bytes)
+}
+
 // DeleteUser deletes user by id
 func DeleteUser(w http.ResponseWriter, r *http.Request) {
 	db := db.GetConnection()
--- routes/user.go
+++ routes/user.go
@@ -7,7 +7,8 @@ import (
 
 func initializeUsersRoutes(r *mux.Router) {
 	s := r.PathPrefix("/users").Subrouter()
-	s.HandleFunc("/{id}", controllers.GetUser).Methods("GET")
 	s.HandleFunc("", controllers.NewUser).Methods("POST")
+	s.HandleFunc("/{id}", controllers.GetUser).Methods("GET")
+	s.HandleFunc("/{id}", controllers.UpdateUser).Methods("PUT")
 	s.HandleFunc("/{id}", controllers.DeleteUser).Methods("DELETE")
 }
