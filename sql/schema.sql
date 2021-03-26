3e682cb1d9b289a211f9a38dbc43cd2a7664676b
Jorge Alfaro <jorge.alfmur@gmail.com>
2021-03-26 11:54:50 -0500

    docker-compose and migration

---
+++ sql/schema.sql
@@ -0,0 +1,9 @@
+CREATE TABLE courses
+(
+    id VARCHAR(40) NOT NULL,
+    username VARCHAR(255) NOT NULL,
+    firstName VARCHAR(255) NOT NULL,
+    lastName VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+) CHARACTER SET utf8mb4
+  COLLATE utf8mb4_bin;
